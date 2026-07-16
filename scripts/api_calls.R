00a6ece4d7f5962e27e70458693a14d80b17daf8
rosiet1039 <rosie.topp@mail.utoronto.ca>
2026-07-15 23:01:03 -0400

    uploading files

---
+++ scripts/api_calls.R
@@ -0,0 +1,7 @@
+#### Preamble ####
+# Purpose: This file contains the code to make the API calls on all models
+# Author: Rosemarie Topp
+# Date: 15 July 2026
+# Contact: rosie.topp@mail.utoronto.ca
+# Pre-requisites: ellmer
+
