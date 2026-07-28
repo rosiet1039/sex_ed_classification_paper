4f2d2e55aff5563fd045baf29ec6c2ed021a22bf
Rosie <rosie.topp@mail.utoronto.ca>
2026-07-27 22:00:00 -0400

    script to turn book into csv file and the finished csv file

---
+++ scripts/create_data.py
@@ -0,0 +1,57 @@
+# Purpose: This file contains the code to create the sex ed dataset
+# Author: Rosemarie Topp
+# Date: 15 July 2026
+# Contact: rosie.topp@mail.utoronto.ca
+# Pre-requisites: Install pdfminer, nltk, re, csv packages
+
+# loading libraries
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextContainer
+import nltk
+import re
+import csv
+nltk.download("punkt")
+nltk.download("punkt_tab")
+
+#extract text from pdf
+pages = range(6, 97)
+def extract_main_text(pdf_path):
+    text = ""
+
+    for page_layout in extract_pages(pdf_path, page_numbers = pages ):
+
+        page_width = page_layout.width
+        page_height = page_layout.height
+
+        for element in page_layout:
+
+            if isinstance(element, LTTextContainer):
+
+                x0, y0, x1, y1 = element.bbox
+
+                #ignoring text in margins
+                if (
+                        x0 > 50 and
+                        x1 < page_width - 50 and
+                        y0 > 50 and
+                        y1 < page_height - 50
+                ):
+                    text += element.get_text()
+
+    return text
+
+text = extract_main_text("../data/its_perfectly_normal.pdf")
+
+#extract unnecessary spaces and newlines
+text = re.sub(r"\n+", " ", text)
+text = re.sub(r"\s+", " ", text).strip()
+
+#tokenize sentences
+sentences = nltk.sent_tokenize(text)
+
+#write tokens into csv file
+with open("../data/sentences.csv", "w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["sentence"])
+    for sentence in sentences:
+        writer.writerow([sentence])
